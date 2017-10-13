a14740f80831be9d8920b66f7545866758d81667
misterned <nedelec.stephane@yahoo.fr>
2017-10-13 18:14:47 +0200

    Add HNArticlesAdapter class

--- app/src/main/java/fr/esaip/nedelec/app0/MainActivity.java
+++ app/src/main/java/fr/esaip/nedelec/app0/MainActivity.java
@@ -1,13 +1,34 @@
 package fr.esaip.nedelec.app0;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.oc.hnapp.android.HNQueryTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HNQueryTask _task = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        HNArticlesAdapter adapter = new HNArticlesAdapter();
+        recyclerView.setAdapter(adapter);
+
+        _task = new HNQueryTask(adapter, 80, 1);
+        _task.execute();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        _task.cancel(true);
     }
 }
